9e98561a7975e790c2cc52c867e8ef58ae4cee51
phoenixvane <32545460+phoenixvane@users.noreply.github.com>
2020-07-23 10:59:33 +0900

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,16 @@
-input.onPinPressed(TouchPin.P0, function () {
-	
+input.onPinPressed(TouchPin.P1, function () {
+    basic.showIcon(IconNames.EigthNote)
+    music.playTone(294, music.beat(BeatFraction.Half))
+    music.playTone(330, music.beat(BeatFraction.Whole))
+    music.playTone(294, music.beat(BeatFraction.Half))
+    music.playTone(330, music.beat(BeatFraction.Whole))
+    music.playTone(523, music.beat(BeatFraction.Half))
+    music.playTone(494, music.beat(BeatFraction.Whole))
+    music.rest(music.beat(BeatFraction.Half))
+    music.playTone(392, music.beat(BeatFraction.Half))
+    basic.clearScreen()
 })
+basic.showString("pitagoras")
 basic.forever(function () {
 	
 })
